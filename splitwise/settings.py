7181539dfcca6b69cf84411575ea1aa1898b1b85
Vamshi Krishna <bommvams72@gmail.com>
2021-09-23 08:50:41 +0530

    Add postman collection and remove Debug option

--- splitwise/settings.py
+++ splitwise/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_!4*j#g130s*o-*7edzy3%jkq#t3vp-i&(7#+4cz&p+p-+g#*b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ["*"]
